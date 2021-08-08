14d0d45d95f145678ee92cf61632c99a46bc810c
Jaylateaux <Piyanat.k.j@gmail.com>
2021-08-08 16:36:54 +0700

    Add sqliteApp Create DataAccess class

---
+++ SQLiteApp/DataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteApp
+{
+    class DataAccess
+    {
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+               new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customers (uid INTEGER PRIMARY KEY, " +
+                    "first_Name CHARACTER(20) NULL, " +
+                    "last_Name CHARACTER(20) NULL, " +
+                    "email NCHAR(55) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
